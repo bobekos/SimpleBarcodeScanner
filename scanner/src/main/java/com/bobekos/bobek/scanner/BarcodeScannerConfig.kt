3a03e9a8febecccfc2989d49decb4371ee1893f9
Mateusz Maziec <matthias.maziec@mintellity.com>
2018-03-27 14:55:25 +0200

    facing

--- scanner/src/main/java/com/bobekos/bobek/scanner/BarcodeScannerConfig.kt
+++ scanner/src/main/java/com/bobekos/bobek/scanner/BarcodeScannerConfig.kt
@@ -1,11 +1,13 @@
 package com.bobekos.bobek.scanner
 
+import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.barcode.Barcode
 
 
 internal data class BarcodeScannerConfig(
         var isAutoFocus: Boolean = true,
         var drawOverLay: Boolean = false,
+        var facing: Int = CameraSource.CAMERA_FACING_BACK,
         var previewSize: Size = Size(640, 480),
         var barcodeFormat: Int = Barcode.ALL_FORMATS
 )
