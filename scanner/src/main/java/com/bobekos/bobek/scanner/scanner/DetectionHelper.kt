b5472ec0b9264038ba0c0811a83b7fd1d15a06d6
Mateusz Maziec <matthias.maziec@mintellity.com>
2018-06-29 13:13:03 +0200

    add beepsound and vibration functionality

---
+++ scanner/src/main/java/com/bobekos/bobek/scanner/scanner/DetectionHelper.kt
@@ -0,0 +1,28 @@
+package com.bobekos.bobek.scanner.scanner
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.ToneGenerator
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+
+
+internal object DetectionHelper {
+
+    fun playBeepSound() {
+        val toneGenerator = ToneGenerator(AudioManager.STREAM_MUSIC, 100)
+        toneGenerator.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+    }
+
+    fun vibrate(ctx: Context?, duration: Long) {
+        if (ctx != null) {
+            val vibrator = ctx.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(duration)
+            }
+        }
+    }
+}
