36cc36674262a32a6b467d0f6b833b92b3fd6ffa
Mateusz Maziec <matthias.maziec@mintellity.com>
2018-07-03 10:37:53 +0200

    recover

--- app/src/main/java/com/bobekos/bobek/simplebarcodescanner/MainActivity.kt
+++ app/src/main/java/com/bobekos/bobek/simplebarcodescanner/MainActivity.kt
@@ -20,31 +20,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        setBtnText()
-//
-//        btnFlash.setOnClickListener {
-//            isFlashOn = !isFlashOn
-//            barcodeView.setFlash(isFlashOn)
-//            setBtnText()
-//        }
-//        btnVibrate.setOnClickListener {
-//            isVibrateOn = !isVibrateOn
-//            barcodeView.setVibration(if (isVibrateOn) 500 else 0)
-//            setBtnText()
-//        }
-//        btnBeep.setOnClickListener {
-//            isBeepOn = !isBeepOn
-//            barcodeView.setBeepSound(isBeepOn)
-//            setBtnText()
-//        }
+        setBtnText()
+
+        btnFlash.setOnClickListener {
+            isFlashOn = !isFlashOn
+            barcodeView.setFlash(isFlashOn)
+            setBtnText()
+        }
+        btnVibrate.setOnClickListener {
+            isVibrateOn = !isVibrateOn
+            barcodeView.setVibration(if (isVibrateOn) 500 else 0)
+            setBtnText()
+        }
+        btnBeep.setOnClickListener {
+            isBeepOn = !isBeepOn
+            barcodeView.setBeepSound(isBeepOn)
+            setBtnText()
+        }
 
     }
 
-//    private fun setBtnText() {
-//        btnFlash.text = String.format("Turn the flashlight %s", if (isFlashOn) "off" else "on")
-//        btnBeep.text = String.format("Turn the beep sound %s", if (isBeepOn) "off" else "on")
-//        btnVibrate.text = String.format("Turn the vibration %s", if (isVibrateOn) "off" else "on")
-//    }
+    private fun setBtnText() {
+        btnFlash.text = String.format("Turn the flashlight %s", if (isFlashOn) "off" else "on")
+        btnBeep.text = String.format("Turn the beep sound %s", if (isBeepOn) "off" else "on")
+        btnVibrate.text = String.format("Turn the vibration %s", if (isVibrateOn) "off" else "on")
+    }
 
     override fun onStart() {
         super.onStart()
